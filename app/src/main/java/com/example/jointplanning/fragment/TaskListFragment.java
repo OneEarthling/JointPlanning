56a29c56a14287f834d51035caabdb0041c926ed
Nadezhda Kazynkina <nadezhda.kaa@gmail.com>
2020-02-20 17:23:00 +0300

    Animation was added; ready menuitem was moved plus refactoring

--- app/src/main/java/com/example/jointplanning/fragment/TaskListFragment.java
+++ app/src/main/java/com/example/jointplanning/fragment/TaskListFragment.java
@@ -1,6 +1,10 @@
 package com.example.jointplanning.fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -55,13 +60,34 @@ public class TaskListFragment extends Fragment {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         Intent intent;
         switch (item.getItemId()) {
             case R.id.ready:
-                item.setChecked(true);
-                intent = ReadyActivity.newIntent(getActivity());
-                startActivity(intent);
+                Drawable drawable = item.getIcon();
+                if(drawable != null) {
+                    drawable.mutate();
+                    drawable.setColorFilter(getResources().getColor(R.color.colorBlack), PorterDuff.Mode.SRC_ATOP);
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(R.string.are_you_ready)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent intent = ReadyActivity.newIntent(getActivity());
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Drawable drawable = item.getIcon();
+                                if(drawable != null) {
+                                    drawable.mutate();
+                                    drawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                                }
+                            }
+                        });
+                builder.create().show();
                 return true;
             case R.id.settings:
                 intent = SettingsActivity.newIntent(getActivity());
