2d971ea337406e21b4aa4c676d230593e4a984d9
nadya <nadezhda.kaa@gmail.com>
2020-03-12 13:04:42 +0300

    Gson is being added; starting to change TaskLab and getting Tasks

---
+++ app/src/main/java/com/example/jointplanning/TaskJsonLab.java
@@ -0,0 +1,55 @@
+package com.example.jointplanning;
+
+import com.example.jointplanning.authorization.BasicCredentials;
+import com.example.jointplanning.model.Task;
+import com.example.jointplanning.model.TaskJson;
+import com.example.jointplanning.rpc.QueryData;
+import com.example.jointplanning.rpc.RPCResult;
+import com.example.jointplanning.rpc.RequestManager;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class TaskJsonLab {
+    private static TaskJsonLab sTaskJsonLab;
+
+    private List<TaskJson> mTasks;
+
+    public static TaskJsonLab get() throws IOException {
+        if (sTaskJsonLab == null) {
+            sTaskJsonLab = new TaskJsonLab();
+        }
+        return sTaskJsonLab;
+    }
+
+    private TaskJsonLab() throws IOException {
+        Gson gson = new Gson();
+        BasicCredentials basicCredentials = new BasicCredentials("demo", "000");
+        QueryData queryData = new QueryData();
+        RPCResult[] rpcResults = RequestManager.rpc(Constants.BASE_URL, basicCredentials.getToken(), "cd_userstory", "Query", queryData);
+        JSONObject[] records = rpcResults[0].result.records;
+        mTasks = gson.fromJson(Arrays.toString(records),  new TypeToken<ArrayList<TaskJson>>(){}.getType());
+    }
+
+    public List<TaskJson> getTasks() {
+        return mTasks;
+    }
+
+    public TaskJson getTask(long id) {
+        for (TaskJson task : mTasks) {
+            if (task.getId() == id) {
+                return task;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/jointplanning/fragment/TaskJsonFragment.java
@@ -0,0 +1,67 @@
+package com.example.jointplanning.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.jointplanning.R;
+import com.example.jointplanning.TaskLab;
+import com.example.jointplanning.model.Task;
+import com.example.jointplanning.model.TaskJson;
+
+
+public class TaskJsonFragment extends Fragment {
+    private static final String ARG_TASK_ID = "task_id";
+
+    private TextView mTextTask;
+    private TextView mEstimate;
+    private TextView mPriority;
+    private TextView mAuthor;
+    private TextView mDateCreated;
+    private TextView mDateEstimated;
+    private TaskJson mTask;
+
+    public static TaskJsonFragment newInstance(long taskId) {
+        Bundle args = new Bundle();
+        args.putLong(ARG_TASK_ID, taskId);
+
+        TaskJsonFragment fragment = new TaskJsonFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        long taskId = getArguments().getLong(ARG_TASK_ID);
+        // получение задачи!!!
+        //mTask = TaskLab.get().getTask(taskId);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        mTextTask = view.findViewById(R.id.task_text);
+        mTextTask.setText(mTask.getCDescription());
+
+        mEstimate = view.findViewById(R.id.task_estimate);
+        mEstimate.setText(String.valueOf(mTask.getNSize()));
+
+        mPriority = view.findViewById(R.id.task_priority);
+        mPriority.setText(String.valueOf(mTask.getNPriority()));
+
+        mAuthor = view.findViewById(R.id.task_author);
+        mAuthor.setText(mTask.getCAuthor());
+
+        mDateCreated = view.findViewById(R.id.task_created_date);
+        mDateCreated.setText(String.valueOf(mTask.getDxCreated()));
+
+        mDateEstimated = view.findViewById(R.id.task_estimated_date);
+        mDateEstimated.setText(String.valueOf(mTask.getDDoneDate()));
+        return view;
+    }
+}
